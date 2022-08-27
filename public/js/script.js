a55f1d043c40356c1f4f3b3d7c80aa95a3daede1
joaopsousaa <104941882+joaopsousaa@users.noreply.github.com>
2022-08-27 13:56:59 +0100

    dev

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,33 @@ document.addEventListener(
   },
   false
 );
+
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
+const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener("submit", (e) => {
+  let li = document.createElement("li");
+  e.preventDefault();
+  //Get message text
+  let msg = e.target.elements.msg.value;
+
+  //Emit message to server
+  socket.emit("chatMessage", msg);
+
+  chatMessages.appendChild(li).append(msg);
+
+  let span = document.createElement("span");
+
+  chatMessages.appendChild(span);
+
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
