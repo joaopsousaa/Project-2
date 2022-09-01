089683ea30e61b48e161871de7249f02ad75b7b2
Nuno Morais <nunodpmorais@gmail.com>
2022-09-01 18:21:46 +0100

    Refactor middlewares

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,3 +1,10 @@
+// ------------------- Middleware ------------------- //
+
+// If there is no userId in the session, redirect to the login page.
 module.exports = (req, res, next) => {
+  const { userId } = req.session;
+
+  if (!userId) return res.redirect("/auth/login");
+
   next();
 };
--- middleware/isLoggedOut.js
+++ middleware/isLoggedOut.js
@@ -1,3 +1,10 @@
+// ------------------- Middleware ------------------- //
+
+// If there is an userId in the session, redirect to the home page.
 module.exports = (req, res, next) => {
+  const { userId } = req.session;
+
+  if (userId) return res.redirect("/");
+
   next();
 };
